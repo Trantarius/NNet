589140a96e05e328bf5b89d87f8e0f7fed44cb5e
Trantarius <leborjt@yahoo.com>
2022-10-03 14:02:38 -0400

    cleanup makenoise

--- tools/makenoise.cpp
+++ tools/makenoise.cpp
@@ -7,109 +7,65 @@
 
 using namespace std::filesystem;
 using Image=png::image<png::rgb_pixel>;
-using Pixel=png::rgb_pixel;
+
+const path orig_dir="val2017";
+const path clean_dir="images";
+const path noise_dir="noiseimages";
 const size_t thread_count=8;
 
-double randf(){
-    return (double)rand()/RAND_MAX;
-}
-
-double randg(){
-    return (randf()+randf()+randf()+randf())-2;
-}
-
-int clamp(int x,int low,int high){
-    return x<low?low:(x>high?high:x);
-}
-
-double clampf(double x,double low,double high){
-    return x<low?low:(x>=high?nexttoward(high,low):x);
-}
-
-
-Pixel randc(Pixel base,double amount){
-    double r=base.red;
-    double g=base.green;
-    double b=base.blue;
-
-    r+=(randg()*amount/2)*r/255.0+(randg()*amount/2);
-    g+=(randg()*amount/2)*g/255.0+(randg()*amount/2);
-    b+=(randg()*amount/2)*b/255.0+(randg()*amount/2);
-
-    base.red   = clamp(r,0,255);
-    base.green = clamp(g,0,255);
-    base.blue  = clamp(b,0,255);
-
-    return base;
-}
-
-
-struct NoiseTask{
+struct ImageTask:public Task{
     double noise_amount;
-    path infile;
-    path outfile;
+    path orig_file;
+    path clean_file;
+    path noise_file;
 
-    NoiseTask(double noise_amount,path infile,path outfile):
-        noise_amount(noise_amount),infile(infile),outfile(outfile){}
+    ImageTask(double noise_amount,path orig_file,path clean_file,path noise_file):
+        noise_amount(noise_amount),orig_file(orig_file),
+        clean_file(clean_file),noise_file(noise_file){}
 
     void perform(){
-        Image image(infile);
-        for(int y=0;y<image.get_height();y++){
-            for(int x=0;x<image.get_width();x++){
+        string convert_cmnd = "magick "+orig_file.string()+" "+clean_file.string();
+        string noise_cmnd   = "magick "+orig_file.string()+" -attenuate "+tostr(noise_amount)+
+                              " +noise Gaussian "+noise_file.string();
+        system(convert_cmnd.c_str());
+        system(noise_cmnd.c_str());
 
-                image[y][x]=randc(image[y][x],noise_amount);
-            }
-        }
-        image.write(outfile);
-    }
+        Image img;
+        img.read(clean_file);
+        remove(clean_file);
+        img.write(clean_file);
 
-    void perform_magick(){
-        path inpng=(infile.parent_path()/infile.stem().concat(".png"));
-        system(("magick "+infile.string()+" "+inpng.string()).c_str());
-        system(("magick "+infile.string()+" -attenuate "+tostr(noise_amount)+" +noise Gaussian "+outfile.string()).c_str());
-        system(("rm "+infile.string()).c_str());
-
-        //fix formatting errors by reading and writing it back. only necessary to suppress warnings.
-        Image img(inpng);
-        system(("rm "+inpng.string()).c_str());
-        img.write(inpng);
-        img.read(outfile);
-        system(("rm "+outfile.string()).c_str());
-        img.write(outfile);
+        img.read(noise_file);
+        remove(noise_file);
+        img.write(noise_file);
     }
 
 };
 
-void do_all(std::list<NoiseTask>* tasks){
-    while(!tasks->empty()){
-        tasks->front().perform_magick();
-        tasks->pop_front();
-    }
-}
-
 int main(){
-    path inpath("images");
-    path outpath("noiseimages");
 
-    if(!exists(outpath)){
-        create_directory(outpath);
+    freopen("/dev/null","w",stderr);
+
+    create_directory(clean_dir);
+    create_directory(noise_dir);
+
+    Threadpool threadpool(thread_count);
+
+    int count=0;
+    for(directory_entry entry:directory_iterator(orig_dir)){
+        if(entry.path().extension()!=string(".jpg")){continue;}
+
+        threadpool.push(new ImageTask(
+            rand()/(double)RAND_MAX,
+            entry.path(),
+            (clean_dir/entry.path().stem()).concat(".png"),
+            (noise_dir/entry.path().stem()).concat(".png")
+        ));
+        count++;
     }
 
-    std::list<NoiseTask> tasks[thread_count];
-    int next_list_idx=0;
-
-    for(directory_entry entry:directory_iterator(inpath)){
-        path outentry=outpath/entry.path().stem().concat(".png");
-        tasks[next_list_idx].push_back(NoiseTask(randf(),entry.path(),outentry));
-        next_list_idx=(next_list_idx+1)%thread_count;
+    while(!threadpool.idle()){
+        print_loadbar((count-threadpool.tasks_left())/(double)count);
     }
-
-    std::thread threads[thread_count];
-
-    for(int n=0;n<thread_count;n++){
-        threads[n]=std::thread(do_all,&(tasks[n]));
-    }
-    for(int n=0;n<thread_count;n++){
-        threads[n].join();
-    }
+    printw(66,"done");
 }
